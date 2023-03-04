700f8f3136a41b1219dd409451cc52268c47cd4c
rijinraju <rijinraju856@gmail.com>
2023-03-04 15:05:35 +0530

    postgres password modified

--- broto_backend/settings.py
+++ broto_backend/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bro_db',
         'USER':'postgres',
-        'PASSWORD':'12345',
+        'PASSWORD':'',
         'HOST':'127.0.0.1',
         'PORT':'',
         "TEST": {
