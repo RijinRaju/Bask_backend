f587ebb2d6036717d147166ecc83b1daf3608b82
rijinraju <rijinraju856@gmail.com>
2023-03-06 23:03:50 +0530

    week is registered

--- Admin/admin.py
+++ Admin/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Users
+from .models import Users,Week
 # Register your models here.
 
-admin.site.register(Users)
+admin.site.register(Users,Week)
